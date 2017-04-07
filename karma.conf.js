6e55c59bc39ac9770e469d7de6c9939681423adb
Yoshiya Hinosawa <stibium121@gmail.com>
2017-04-07 14:35:41 +0900

    chore: switch to firefox test

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['PhantomJS'],
+    browsers: ['Firefox'],
     singleRun: true
   })
 }
